7e2bb88f9daad71be7ddfabf26a85a3ca11e58ef
aryansrivv <aryansrivv@gmail.com>
2024-10-05 21:40:01 +0530

    Initial Commit

---
+++ bg/bgApp.swift
@@ -0,0 +1,17 @@
+//
+//  bgApp.swift
+//  bg
+//
+//  Created by Aryan Srivastava on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct bgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bgTests/bgTests.swift
@@ -0,0 +1,17 @@
+//
+//  bgTests.swift
+//  bgTests
+//
+//  Created by Aryan Srivastava on 05/10/24.
+//
+
+import Testing
+@testable import bg
+
+struct bgTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
